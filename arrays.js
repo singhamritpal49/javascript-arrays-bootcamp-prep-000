b4cb54f7a506ce886832efbaa2faec08a8ccd224
Amritpal Singh <singhamritpal49@gmail.com>
2019-04-07 03:24:14 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -30,6 +30,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.shift(array);
-
+  array.slice(1);
+  return array
 }
